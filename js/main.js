aaa55f2e9961276fcd24e02ab4293c73caf1a3a0
padams12 <piddyadams@gmail.com>
2022-08-22 21:16:56 -0700

    Add JavaScript logic to iterate over book collection.

--- js/main.js
+++ js/main.js
@@ -6,14 +6,30 @@ function Book(title, author, pages){
         this.author = author;
         this.pages = pages;
 
-
 }
 
 function addBookToLibrary(){
 
-    let bookTitle = alert("Please enter the book title: "); //User enters book title.
-    let bookAuthor = alert("Please enter the book's author: "); //User enters book author.
-    let bookPages = alert("Please enter the number of pages the book has: "); //User enters number of pages.
-    let newBook = Book(bookTitle, bookAuthor, bookPages); //Call new book function and pass user information to it.
+    let bookTitle = prompt("Please enter the book title: "); //User enters book title.
+    let bookAuthor = prompt("Please enter the book's author: "); //User enters book author.
+    let bookPages = prompt("Please enter the number of pages the book has: "); //User enters number of pages.
+    let newBook = new Book(bookTitle, bookAuthor, bookPages); //Call new book function and pass user information to it.
     myLibrary.push(newBook); //Push the new book object to the library collection array.
+
+}
+
+function displayLibraryCollection() {
+
+
+    for (i=0; i < myLibrary.length; i++) {
+
+        let book = myLibrary[i]; //Set book equal to current array item (Book object).
+        let title = book.title; //Set title.
+        let author = book.author; //Set author.
+        let pages = book.pages; //Set pages.
+        let message = `${title} by ${author}, ${pages} pages long.`; //Set return message.
+        return message;
+
+    }
+
 }
